45bc0703a9edeb275d4e985b3690f9019e1a6d5f
VasMax10 <60179065+VasMax10@users.noreply.github.com>
2023-05-27 23:48:26 +0300

    .

--- src/SentimentAnalyzerParcs.java
+++ src/SentimentAnalyzerParcs.java
@@ -58,13 +58,12 @@ public class SentimentAnalyzerParcs implements AM {
 
     public void run(AMInfo info) {
         Input input = (Input) info.parent.readObject();
-        
+
         String text = input.getText();
         String positive = input.getPositive();
+        String negative = input.getNegative();
 
-//        String negative = input.getNegative();
-
-        System.out.println("Input : text = " + text + ", positive = " + positive);
+        System.out.println("Input : text = " + text + ", positive = " + positive + ", negative = " + negative);
 
         info.parent.write(search(text, positive));
     }
