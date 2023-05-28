434fe50ecf8a462ef47e2f5455eb364b450624c6
VasMax10 <60179065+VasMax10@users.noreply.github.com>
2023-05-28 23:25:19 +0300

    .

--- src/Solver.java
+++ src/Solver.java
@@ -13,7 +13,7 @@ public class Solver {
         task curtask = new task();
         curtask.addJarFile("SentimentAnalyzerParcs.jar");
 
-        int nThreads = 3;
+        int nThreads = 2;
         String text = textFromFile(curtask.findFile("Monte-Cristo.txt"));
 
         String positive = sentimentFromFile(curtask.findFile("positive_words2.txt"));
