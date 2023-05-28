088b1bb6120b1c5258b3e2869cb7086da0c330ee
VasMax10 <60179065+VasMax10@users.noreply.github.com>
2023-05-28 22:52:39 +0300

    .

--- src/SentimentAnalyzerParcs.java
+++ src/SentimentAnalyzerParcs.java
@@ -12,13 +12,11 @@ public class SentimentAnalyzerParcs implements AM {
         int negativeCount = 0;
 
         for (String sentence : sentences){
-            // Convert the sentence to lowercase for case-insensitive matching
+
             sentence = sentence.toLowerCase();
 
-            // Split the sentence into words
             String[] words = sentence.split("[ ,\"']");
 
-            // Iterate through each word and check if it is a positive or negative word
             for (String word : words) {
                 if (positiveWords.contains(word)) {
                     positiveCount++;
@@ -26,17 +24,8 @@ public class SentimentAnalyzerParcs implements AM {
                     negativeCount++;
                 }
             }
-
-//            // Determine the sentiment based on the counts
-//            System.out.println(positiveCount + " " + negativeCount);
-//            if (positiveCount > negativeCount) {
-//                return "Positive";
-//            } else if (negativeCount > positiveCount) {
-//                return "Negative";
-//            } else {
-//                return "Neutral";
-//            }
         }
+
         result.setNegativeCount(negativeCount);
         result.setPositiveCount(positiveCount);
 
@@ -51,93 +40,8 @@ public class SentimentAnalyzerParcs implements AM {
         List<String> positive = input.getPositive();
         List<String> negative = input.getNegative();
 
-        System.out.println("Input : text = " + sentences.get(0) + ", positive = " + positive + ", negative = " + negative);
+        System.out.println("Input : sentences count = " + sentences.size());
 
         info.parent.write(AnalyzeSentiment(sentences, positive, negative));
     }
 }
-
-//
-//
-//
-//import java.util.*;
-//
-//import parcs.*;
-//
-//public class SentimentAnalyzerParcs {
-//    public void run(AMInfo info) {
-//
-//        System.out.println("Start");
-//
-//        Input input = (Input) (info.parent.readObject());
-//
-//        System.out.println("Input : text = " + input.getText() + ", negative = " + input.getNegative() + ", positive = " + input.getPositive());
-//
-//        info.parent.write(input);
-//    }
-//}
-////
-////public class SentimentAnalyzerParcs implements AM {
-////    @Override
-////    public void run(AMInfo amInfo) {
-////
-////        System.out.println("Started");
-////
-////        Input input = (Input) amInfo.parent.readObject() ;
-////
-//////        String input = (String) amInfo.parent.readObject();
-//////        int input = amInfo.parent.readInt();
-////        System.out.println("Got Input");
-////
-//        List<String> negativeWords = new ArrayList<>(input.getNegativeWords());
-//        System.out.println("Negative");
-//////        List<String> positiveWords = new ArrayList<>(input.getPositiveWords());
-//////        System.out.println("Positive");
-////        List<String> sentences     = new ArrayList<>(input.getSentences());
-////        System.out.println("Sentences");
-////
-////        for (String sentence: sentences){
-////            System.out.println(sentence);
-////        }
-////        Result result = new Result();
-////        amInfo.parent.write(result);
-//////        amInfo.parent.write(analyzeAllSentiments(sentences, positiveWords, negativeWords));
-////    }
-////
-////    public Result analyzeAllSentiments(List<String> sentences,
-////                                   List<String> positiveWords,
-////                                   List<String> negativeWords)
-////    {
-////        Result result = new Result();
-////
-////        for (String sentence : sentences){
-////            analyzeSentiment(result, sentence, positiveWords, negativeWords);
-////        }
-////
-////        return result;
-////    }
-////
-////    public static void analyzeSentiment(Result result, String sentence, List<String> positiveWords, List<String> negativeWords) {
-////        int positiveCount = 0;
-////        int negativeCount = 0;
-////
-////        // Convert the sentence to lowercase for case-insensitive matching
-////        sentence = sentence.toLowerCase();
-////
-////        // Split the sentence into words
-////        String[] words = sentence.split("[ ,\"']");
-////
-////        // Iterate through each word and check if it is a positive or negative word
-////        for (String word : words) {
-////            if (positiveWords.contains(word)) {
-////                positiveCount++;
-////            } else if (negativeWords.contains(word)) {
-////                negativeCount++;
-////            }
-////        }
-////
-////        result.incTotalNegative(negativeCount);
-////        result.incTotalPositive(positiveCount);
-////
-////    }
-////}
--- src/Solver.java
+++ src/Solver.java
@@ -9,16 +9,19 @@ import java.util.Scanner;
 public class Solver {
 
     public static void main(String[] args) throws Exception {
+
         task curtask = new task();
         curtask.addJarFile("SentimentAnalyzerParcs.jar");
 
-        String text = textFromFile( curtask.findFile("Moby-Dick") );
-        String positive = sentimentFromFile( curtask.findFile("positive_words2.txt"));
-        String negative = sentimentFromFile( curtask.findFile("negative_words2.txt"));
+        int nThreads = 4;
+        String text = textFromFile(curtask.findFile("Moby-Dick"));
+
+        String positive = sentimentFromFile(curtask.findFile("positive_words2.txt"));
+        String negative = sentimentFromFile(curtask.findFile("negative_words2.txt"));
 
         AMInfo info = new AMInfo(curtask, null);
 
-        int nThreads = 4;
+        long tStart = System.nanoTime();
 
         List<point> points = new ArrayList<>();
         List<channel> channels = new ArrayList<>();
@@ -59,7 +62,7 @@ public class Solver {
             System.out.println("Waiting for result .. ");
         }
 
-        for (int i = 0; i < nThreads; i++){
+        for (int i = 0; i < nThreads; i++) {
             Result result = (Result) (channels.get(i).readObject());
 
             Integer posCount = result.getPositiveCount();
@@ -69,6 +72,9 @@ public class Solver {
 
         }
 
+        long tEnd = System.nanoTime();
+
+        System.out.println("Working time on " + nThreads + " processes: " + ((tEnd - tStart) / 1000000) + "ms");
 
         curtask.end();
     }
@@ -79,6 +85,7 @@ public class Solver {
 
         return text;
     }
+
     public static String sentimentFromFile(String filename) throws Exception {
         String pattern = "";
 
@@ -107,7 +114,7 @@ public class Solver {
                 endIndex = listSize;
             }
 
-            if (mod > 0){
+            if (mod > 0) {
                 endIndex++;
                 mod--;
             }
@@ -123,313 +130,3 @@ public class Solver {
     }
 
 }
-
-
-
-//import  java.io.*;
-//import java.util.*;
-//
-//import parcs.*;
-//
-//public class Solver {
-//    public static void main(String[] args) throws Exception {
-//        task curtask = new task();
-//        curtask.addJarFile("SentimentAnalyzerParcs.jar");
-//
-//        String text = textFromFile(curtask.findFile("input_small.txt"));
-//        String positive = textFromFile(curtask.findFile("positive_words.txt"));
-//        String negative = textFromFile(curtask.findFile("negative_words.txt"));
-//
-//        AMInfo info = new AMInfo(curtask, null);
-//
-//        int nThreads = 4;
-//
-//        List<point> points = new ArrayList<>();
-//        List<channel> channels = new ArrayList<>();
-//
-//        for (int i = 0; i < nThreads; i++) {
-////            String t = texts.get(i);
-////            Integer shift = shifts.get(i);
-//
-//            point p = info.createPoint();
-//            channel c = p.createChannel();
-//
-//            points.add(p);
-//            channels.add(c);
-//
-//            Input input = new Input(positive, negative, text);
-//
-//            p.execute("SentimentAnalyzerParcs");
-//            c.write(input);
-//
-//            System.out.println(input.getText());
-//            System.out.println(input.getNegative());
-//            System.out.println(input.getPositive());
-//            System.out.println("Waiting for result .. ");
-//
-////            Result result = (Result) (c.readObject());
-////            String result = (String) (c.readObject());
-//            Input result = (Input) (c.readObject());
-//            System.out.println(result.getText());
-//            System.out.println(result.getNegative());
-//            System.out.println(result.getPositive());
-////            System.out.println(result);
-////            List<Integer> ins = result.getRes();
-////            if (ins.size() > 0) {
-////                System.out.println("Pattern ins : {");
-////                for (int index : ins) {
-////                    System.out.print(shift + index + " ");
-////                }
-////                System.out.println("}");
-////            }
-//        }
-//
-//
-//        curtask.end();
-//    }
-//    public static String textFromFile(String filename) throws Exception {
-//
-//        String text = new Scanner(new File(filename)).useDelimiter("\\Z").next();
-//
-//        return text;
-//    }
-//}
-////
-////public class Solver{
-////    public static void main(String[] args) throws Exception {
-////        task curtask = new task();
-////        curtask.addJarFile("SentimentAnalyzerParcs.jar");
-////
-////        int nThreads;
-////
-////        StringBuilder full_text = new StringBuilder();
-////
-////        try
-////        {
-////            BufferedReader in = new BufferedReader(new FileReader(curtask.findFile("input_small.txt")));
-////
-////            nThreads = Integer.parseInt(in.readLine());
-////            String line;
-////
-////            while ((line = in.readLine()) != null) {
-////                full_text.append(line);
-////            }
-////        }
-////        catch (IOException e)
-////        {
-////            System.out.print("Reading input data error\n");
-////            e.printStackTrace();
-////            return;
-////        }
-////
-////        List<String> positiveWords = readListFromFile(curtask.findFile("positive_words.txt"));
-////        List<String> negativeWords = readListFromFile(curtask.findFile("negative_words.txt"));
-////
-////        AMInfo info = new AMInfo(curtask, null);
-////
-//        List<String> sentences = List.of(full_text.toString().split("[.!?]"));
-//        List<String> non_empty_sentences = new ArrayList<>();
-//        int count = 0;
-//
-//        for (String sentence : sentences) {
-//            if (!sentence.trim().isEmpty()) {
-//                count++;
-//                non_empty_sentences.add(sentence);
-//            }
-//        }
-//
-//        System.out.println("Number of sentences: " + count);
-////
-////        List<List<String>> dividedLists = divideList(non_empty_sentences, nThreads);
-////
-////
-////        List<point> points = new ArrayList<>();
-////        List<channel> channels = new ArrayList<>();
-////
-////        for (int i = 0; i < nThreads; i++){
-////
-////            point p = info.createPoint();
-////            channel c = p.createChannel();
-////
-////            points.add(p);
-////            channels.add(c);
-////
-////            Input input = new Input(
-//////                    positiveWords, negativeWords,
-////                    dividedLists.get(i));
-////
-////            p.execute("SentimentAnalyzerParcs");
-////            c.write(input);
-////
-////            System.out.println("Waiting for result .. ");
-////
-//////            Result result = (Result) (c.readObject());
-////
-//////            System.out.println(result.getTotalNegative());
-//////            System.out.println(result.getTotalPositive());
-////        }
-////        curtask.end();
-////    }
-////
-//////public class Solver implements AM{
-//////
-//////    public static void main(String[] args){
-//////        task curtask = new task();
-//////
-//////        curtask.addJarFile("SentimentAnalyzerParcs.jar");
-//////
-//////        System.out.println("The Solver class method 'main' has added jar file 'SentimentAnalyzerParcs.jar'");
-//////
-//////        (new Solver()).run(new AMInfo(curtask, null));
-//////
-//////        System.out.println("The Solver class method 'main' has finished its work");
-//////        curtask.end();
-//////    }
-//////
-//////    @Override
-//////    public void run(AMInfo amInfo) {
-//////
-//////        int nThreads;
-//////
-//////        StringBuilder full_text = new StringBuilder();
-//////
-//////        try
-//////        {
-//////            BufferedReader in = new BufferedReader(new FileReader(amInfo.curtask.findFile("input_small.txt")));
-//////
-//////            nThreads = Integer.parseInt(in.readLine());
-//////            String line;
-//////
-//////            while ((line = in.readLine()) != null) {
-//////                full_text.append(line);
-//////            }
-//////        }
-//////        catch (IOException e)
-//////        {
-//////            System.out.print("Reading input data error\n");
-//////            e.printStackTrace();
-//////            return;
-//////        }
-//////
-//////        List<String> positiveWords = readListFromFile(amInfo, "positive_words.txt");
-//////        List<String> negativeWords = readListFromFile(amInfo,"negative_words.txt");
-//////
-//////        List<String> sentences = List.of(full_text.toString().split("[.!?]"));
-//////        List<String> non_empty_sentences = new ArrayList<>();
-//////        int count = 0;
-//////
-//////        for (String sentence : sentences) {
-//////            if (!sentence.trim().isEmpty()) {
-//////                count++;
-//////                non_empty_sentences.add(sentence);
-//////            }
-//////        }
-//////
-//////        System.out.println("Number of sentences: " + count);
-//////
-//////        List<List<String>> dividedLists = divideList(non_empty_sentences, nThreads);
-//////
-//////        List<point> points = new ArrayList<point>();
-//////        List<channel> channels = new ArrayList<channel>();
-//////
-//////        // Connection to points
-//////
-//////        // Time counting
-//////        long tStart = System.nanoTime();
-//////
-//////        int totalNegative = 0;
-//////        int totalPositive = 0;
-//////
-//////        for(int i = 0; i < nThreads; i++)
-//////        {
-//////            System.out.println(i);
-//////
-//////            point p = amInfo.createPoint();
-//////            channel c = p.createChannel();
-//////
-//////            points.add(p);
-//////            channels.add(c);
-//////
-//////            Input input = new Input(positiveWords, negativeWords, dividedLists.get(i));
-//////
-//////            System.out.println(positiveWords.get(0));
-//////            System.out.println(negativeWords.get(0));
-//////            System.out.println(dividedLists.get(i).get(0));
-//////
-//////            p.execute("SentimentAnalyzerParcs");
-//////            c.write(123);
-//////
-//////            System.out.println("Waiting for the result...");
-//////
-//////            Result result = (Result) (c.readObject());
-//////
-//////            totalPositive += result.getTotalPositive();
-//////            totalNegative += result.getTotalNegative();
-//////
-//////        }
-//////
-//////        long tEnd = System.nanoTime();
-//////
-//////        if (totalPositive > totalNegative) {
-//////            System.out.println("Positive");
-//////        } else if (totalNegative > totalPositive) {
-//////            System.out.println("Negative");
-//////        } else {
-//////            System.out.println("Neutral");
-//////        }
-//////
-//////        System.out.println("Working time on " + nThreads + " processes: " + ((tEnd - tStart) / 1000000) + "ms");
-//////    }
-//////
-////    public static List<List<String>> divideList(List<String> originalList, int numberOfDivisions) {
-////        List<List<String>> dividedLists = new ArrayList<>();
-////
-////        int listSize = originalList.size();
-////        int sublistSize = listSize / numberOfDivisions;
-////        int mod = listSize % numberOfDivisions;
-////
-////        System.out.println("mod = " + mod);
-////
-////        int startIndex = 0;
-////        int endIndex = sublistSize;
-////
-////        for (int i = 0; i < numberOfDivisions; i++) {
-////            if (i == numberOfDivisions - 1) {
-////                // Last sublist may have different size
-////                endIndex = listSize;
-////            }
-////
-////            if (mod > 0){
-////                endIndex++;
-////                mod--;
-////            }
-////
-////            List<String> sublist = originalList.subList(startIndex, endIndex);
-////            dividedLists.add(sublist);
-////
-////            startIndex = endIndex;
-////            endIndex += sublistSize;
-////        }
-////
-////        return dividedLists;
-////    }
-////
-////    public static List<String> readListFromFile(String file){
-////
-////        List<String> lines = new ArrayList<>();
-////
-////        try {
-////            BufferedReader in = new BufferedReader(new FileReader(file));
-////            String line;
-////
-////            while ((line = in.readLine()) != null) {
-////                lines.add(line);
-////            }
-////
-////        } catch (IOException e) {
-////            e.printStackTrace();
-////            System.out.print("Reading sentiment words error\n");
-////        }
-////        return lines;
-////    }
-////}
