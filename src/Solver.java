d79f552b460a67083620e40620c1a6b2d05aba89
VasMax10 <60179065+VasMax10@users.noreply.github.com>
2023-05-29 12:00:49 +0300

    .

--- src/Solver.java
+++ src/Solver.java
@@ -13,7 +13,7 @@ public class Solver {
         task curtask = new task();
         curtask.addJarFile("SentimentAnalyzerParcs.jar");
 
-        int nThreads = 1;
+        int nThreads = 2;
 
 //        String text = textFromFile(curtask.findFile("Moby-Dick.txt"));
 //        List<String> sentences = List.of(text.split("[.!?]"));
