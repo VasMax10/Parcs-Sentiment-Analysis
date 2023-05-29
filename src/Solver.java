35c6b4ce0b1b5ebedb1d049a5b04750a34a2ba99
VasMax10 <60179065+VasMax10@users.noreply.github.com>
2023-05-29 11:53:59 +0300

    .

--- src/Solver.java
+++ src/Solver.java
@@ -13,7 +13,7 @@ public class Solver {
         task curtask = new task();
         curtask.addJarFile("SentimentAnalyzerParcs.jar");
 
-        int nThreads = 2;
+        int nThreads = 4;
 
         String text = textFromFile(curtask.findFile("input_small.txt"));
         List<String> sentences = List.of(text.split("[.!?]"));
